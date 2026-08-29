731f7d5d2425c63e7a6c718f7dd11a09bd920244
donardi-1997 <adrianguerra9703@gmail.com>
2026-08-28 19:20:28 -0500

    Allow root production domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -93,6 +93,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://diaglob.tech",
+        "https://www.diaglob.tech",
         "https://app.diaglob.tech",
     ],
     allow_credentials=True,
