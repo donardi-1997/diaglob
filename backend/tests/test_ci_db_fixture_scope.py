c5ea50994b0a59abee9ee0f9f0dd55c63a071df5
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-11 14:36:13 -0500

    test: require Analytics schema reuse with row cleanup

--- backend/tests/test_ci_db_fixture_scope.py
+++ backend/tests/test_ci_db_fixture_scope.py
@@ -141,3 +141,7 @@ def test_payments_schema_is_created_once_and_data_cleanup_has_no_ddl():
 
 def test_whatsapp_schema_is_created_once_and_data_cleanup_has_no_ddl():
     _assert_schema_once_with_cleanup(ROOT / "test_whatsapp.py", "setup_db")
+
+
+def test_analytics_schema_is_created_once_and_data_cleanup_has_no_ddl():
+    _assert_schema_once_with_cleanup(ROOT / "test_analytics.py", "setup_db")
